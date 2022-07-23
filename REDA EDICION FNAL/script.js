2e197315c69729364e465c39977f179b6b724c3e
Fabian1Munoz <103016185+Fabian1Munoz@users.noreply.github.com>
2022-07-23 17:08:09 -0500

    Update script.js

--- REDA EDICION FNAL/script.js
+++ REDA EDICION FNAL/script.js
@@ -24,4 +24,32 @@ const preguntas = [
     respuestaCorrecta: "b",
   },
 
+ {
+    pregunta: "3: ¿En la fracción 5/6 cuál numeron es el denominador?",
+    respuestas: {
+      a: "el número 5",
+      b: "el número 3",
+      c: "el número 6",  
+      d:"Todas son correctas",
+    },
+    respuestaCorrecta: "c",
+  },
+  {
+    pregunta: "4: ¿ para restar dos fracciones se debe invertir la segunda fracción y luego restar?",
+    respuestas: {
+      a: "verdadero",
+      b: "falso",
+    },
+    respuestaCorrecta: "b",
+  },
+  {
+    pregunta: "5: Juanito camino 3/2 km. Y al otro día camino 5/2 de  km ¿Cuánto camino en los dos días?",
+    respuestas: {
+      a: "8 Km",
+      b: "6 Km",
+      c: "4 Km",
+
+    },
+    respuestaCorrecta: "c",
+  },
 

45199530792e4df04d09b8bfa7e19b7332e087e1
Fabian1Munoz <103016185+Fabian1Munoz@users.noreply.github.com>
2022-07-23 17:07:16 -0500

    Update script.js

--- REDA EDICION FNAL/script.js
+++ REDA EDICION FNAL/script.js
@@ -2,4 +2,26 @@ const contenedor = document.getElementById("test");
 const botonRes = document.getElementById("boton");
 const resultadoTest = document.getElementById("resultado");
 
+const preguntas = [
+ {
+    pregunta: "1: ¿En la fracción ¾  el numerador es el número?",
+    respuestas: {
+    a: "A: el número 3",
+    b: "el número 4",
+	c: "Ninguno de los anteriores", 
+    
+    },
+    respuestaCorrecta: "b",
+  },
+  {
+    pregunta: "2:¿Cómo se escribe la fracción cinco medio?",
+    respuestas: {
+     a:"5/5" ,
+     b:"5/2" ,
+     c:"2/5" ,
+
+    },
+    respuestaCorrecta: "b",
+  },
+
 
